480309a197661284dc8ab3e7cc0a3d142a0a3893
Jeremias Juarez Marrades <jeremiasjuarezmarrades15@hotmail.com>
2022-12-18 22:09:16 -0300

    2da entrega

--- javascript/js.js
+++ javascript/js.js
@@ -1,101 +1,53 @@
-let pepsi = 1900;
-let mirinda = 1900;
-let seven = 1900;
-let seven_free = 1900;
-let coca_cola2 = 2200;
-let fanta2 = 2200;
-let sprite2 = 2200;
-let secco3 = 950;
+let total=0;
+let cantidad=0;
+let total_pagar=0;
+const lista_productos=[ 
+    { Bebidas:"pepsi" , Precio: 1950},
+    { Bebidas:"mirinda", Precio: 2000},
+    { Bebidas:"seven" , Precio: 2200},
+    { Bebidas:"coca cola", Precio: 2300},
+    { Bebidas:"fanta" , Precio: 2350},
+    { Bebidas:"sprite", Precio: 2400},
+    { Bebidas:"secco" , Precio: 2550},
+    { Bebidas:"torasso", Precio: 2600},
 
-//
-let totalpepsi = 0;
-let totalmirinda = 0;
-let totalseven = 0;
-let totalseven_free = 0;
-let totalcoca_cola2 = 0;
-let totalfanta2 = 0;
-let totalsprite2 = 0;
-let totalsecco3 = 0;
-let cantidad = 0;
-
-
-function calcular_total(){
-    
-    let total=  totalpepsi+totalmirinda+totalseven+totalseven_free+totalcoca_cola2+totalfanta2+totalsprite2+ totalsecco3;
-    console.log("total a pagar: $", total); 
-}
-
-
+]
 
 console.log( "Bienvenidos");
 
-
- let producto = parseInt(prompt("Elija el o los producto que desecea comprar. Para ver los productos y sus precios presione el numero 1"));
-
+let inicio= parseInt(prompt("Presione el numero 1 para iniciar su compra y ver el listado de productos"));
 
 
- while( producto == 1){
+while(inicio==1){ 
 
-    console.log("Pepsi $1900");
-    console.log("Mirinda $1900");
-    console.log("Seven $1900");
-    console.log("Seven Free $1900");
-    console.log("Coca Cola 2L $2200");
-    console.log("Fanta 2L $2200");
-    console.log("Sprite 2L $2200");
-    console.log("Secco 3L $950");  
+    console.log(lista_productos);
 
-    bebida = prompt("Para elejir la bebida que quiere comprar escriba el nombre de la misma");
-    bebida=bebida.toLowerCase();
-    while(bebida == "pepsi" || bebida == "mirinda" || bebida == "seven" || bebida == "sevenfree" || bebida == "cocacola" || bebida == "fanta" || bebida == "sprite" || bebida == "secco" && bebida !=0 ){
-        
-        
-        cantidad = parseInt(prompt("Ingrese la cancidad que desea comprar"));
+function buscar_producto(lista_producto){
+    return lista_producto.Bebidas == compra_usuario;
+}
 
-        if(bebida=="pepsi"){
-            totalpepsi = pepsi * cantidad;
-        }
+let compra_usuario = prompt("Ingrese el nombre del producto que desea comprar");
+compra_usuario=compra_usuario.toLowerCase();
 
-        if(bebida=="mirinda"){
-            totalmirinda = mirinda * cantidad;
-        }
+let resultado_find = lista_productos.find(buscar_producto);
 
-        if(bebida=="seven"){
-            totalseven = seven * cantidad;
-        }
+if(resultado_find != undefined){
+   
+    cantidad = parseInt(prompt("Elija la cantidad que desea comprar"));
+    precios=lista_productos.Precio;
 
-        if(bebida=="sevenfree"){
-            totalseven_free = seven_free * cantidad;
-        }
-
-        if(bebida=="cocacola"){
-            totalcoca_cola2 = coca_cola2 * cantidad;
-        }
-
-        if(bebida=="fanta"){
-            totalfanta2 = fanta2 * cantidad;
-        }
-
-        if(bebida=="sprite"){
-            totalsprite2 = sprite2 * cantidad;
-        }
-
-        if(bebida=="secco"){
-            totalsecco3 = secco3 * cantidad;
-        }
-
+    total=resultado_find.Precio*cantidad;
+    
+    total_pagar=total_pagar+total;
+  
+}
+else{
+    console.log("No tenes el producto que desea comprar")   
      
-
-        bebida = prompt("Si quiere elegir otra bebida escriba el nombre, de lo contrario presione el numero 0");
-    }
-
-    producto = parseInt(prompt("Si desea comprar otra cosa precione 1 sino precione 0"));
-     
-}; 
-calcular_total();
-
- console.log("Muchas Gracias por su Compra");
+}
 
 
-
-
+inicio=parseInt(prompt("Si desea Finalizar su compra precione el numero 0, si desea seguir comprando precione el numero 1"));
+}
+console.log("Total a pagar:", total_pagar);
+console.log("Gracias Por su Compra");
